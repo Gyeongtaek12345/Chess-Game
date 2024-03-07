c9580ff4c0031eb2390e192b82140e93edbb5124
Gyeongtaek <ogt124321@gmail.com>
2024-03-08 00:32:34 +0900

    Position 객체 추가 및 Piece 관련 리팩토링 piece 생성자에 피스들의 위치 값을 추가하여 각각의 피스들이 보드의 위치 값을 가질수 있도록 만들어주었습니다.

--- src/main/java/pieces/Piece.java
+++ src/main/java/pieces/Piece.java
@@ -23,14 +23,17 @@ public class Piece {
     }
     private final PieceColor teamColor;
     private final PieceType pieceType;
-    private Piece(PieceColor color, PieceType pieceType){
+    private final Position position;
+    private Piece(PieceColor color, PieceType pieceType, Position position){
         this.teamColor = color;
         this.pieceType = pieceType;
+        this.position = position;
     }
     public PieceColor getColor(){
         return teamColor;
     }
     public PieceType getPieceType(){return pieceType;}
+    public Position getPosition(){return position;}
     public boolean isWhite(){
         return teamColor.equals(WHITE);
     }
@@ -42,43 +45,59 @@ public class Piece {
         }
     }
     // 팩토리 메서드
-    public static Piece createWhitePawn() {
-        return new Piece(WHITE, PAWN);
+    public static Piece createWhitePawn(Position position) {
+        return new Piece(WHITE, PAWN,position);
     }
-    public static Piece createBlackPawn() {
-        return new Piece(BLACK, PAWN);
+    public static Piece createBlackPawn(Position position) {
+        return new Piece(BLACK, PAWN,position);
     }
-    public static Piece createWhiteKnight() {
-        return new Piece(WHITE, KNIGHT);
+    public static Piece createWhiteKnight(Position position) {
+        return new Piece(WHITE, KNIGHT,position);
     }
-    public static Piece createBlackKnight() {
-        return new Piece(BLACK, KNIGHT);
+    public static Piece createBlackKnight(Position position) {
+        return new Piece(BLACK, KNIGHT,position);
     }
-    public static Piece createWhiteRook() {
-        return new Piece(WHITE, ROOK);
+    public static Piece createWhiteRook(Position position) {
+        return new Piece(WHITE, ROOK,position);
     }
-    public static Piece createBlackRook() {
-        return new Piece(BLACK, ROOK);
+    public static Piece createBlackRook(Position position) {
+        return new Piece(BLACK, ROOK,position);
     }
-    public static Piece createWhiteBishop() {
-        return new Piece(WHITE, BISHOP);
+    public static Piece createWhiteBishop(Position position) {
+        return new Piece(WHITE, BISHOP,position);
     }
-    public static Piece createBlackBishop() {
-        return new Piece(BLACK, BISHOP);
+    public static Piece createBlackBishop(Position position) {
+        return new Piece(BLACK, BISHOP,position);
     }
-    public static Piece createWhiteQueen() {
-        return new Piece(WHITE, QUEEN);
+    public static Piece createWhiteQueen(Position position) {
+        return new Piece(WHITE, QUEEN,position);
     }
-    public static Piece createBlackQueen() {
-        return new Piece(BLACK, QUEEN);
+    public static Piece createBlackQueen(Position position) {
+        return new Piece(BLACK, QUEEN,position);
     }
-    public static Piece createWhiteKing() {
-        return new Piece(WHITE, KING);
+    public static Piece createWhiteKing(Position position) {
+        return new Piece(WHITE, KING,position);
     }
-    public static Piece createBlackKing() {
-        return new Piece(BLACK, KING);
+    public static Piece createBlackKing(Position position) {
+        return new Piece(BLACK, KING,position);
     }
-    public static Piece createBlank() {
-        return new Piece(NO_COLOR, NO_PIECE);
+    public static Piece createBlank(Position position) {
+        return new Piece(NO_COLOR, NO_PIECE,position);
+    }
+    @Override
+    public String toString() {
+        return this.pieceType.toString();
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Piece piece = (Piece) obj;
+        return pieceType == piece.pieceType && teamColor == piece.teamColor;
     }
 }
+
